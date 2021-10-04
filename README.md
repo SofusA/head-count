36a367b3fa3db395df65f33bebab576d64e6eec3
Sofus Addington <sofus@addington.dk>
2021-10-04 16:44:58 +0200

    Add export, monitor and status

--- README.md
+++ README.md
@@ -3,10 +3,12 @@
 Visitor counting system in Node / sqlite
 
 ## Todo
-- [X ] Migration tool from sqlite to supabase. This should ensure that copying is unique.
-- [ ] Serve static html on Cloud Run server
-- [ ] Move all colour from CSS to HTML, and change with JS - smart
-- [ ] Create the "status" page
-- [ ] Create monitoring page
-- [ ] Create export page
+- [X] Migration tool from sqlite to supabase. This should ensure that copying is unique.
+- [X] Serve static html on Cloud Run server
+- [X] Move all colour from CSS to HTML, and change with JS - smart
+- [X] Create the "status" page
+- [X] Create monitoring page
+- [X] Create export page
+- [X] Add restart.js to new pages
+- [ ] helmet.js
 - [ ] Go live!
---
+++ cloudrun-server/sites/subscribe.js
@@ -0,0 +1,179 @@
+const supabaseUrl = 'https://flpcdxxaexyriudizhty.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyNjQyNzkyOCwiZXhwIjoxOTQyMDAzOTI4fQ.XoT5qhcfNYapF_rDJxzokPX9eOGCB0hzBW5crHvcnoA'
+const db = supabase.createClient(supabaseUrl, supabaseKey)
+
+let DateTime = luxon.DateTime;
+
+const subscribe = (location, elements) => {
+    return db
+        .from(`counter:location=eq.${location}`) //only listen to test location
+        .on('*', payload => {
+            initiatePage(location, elements)
+        })
+        .subscribe()
+}
+
+// const handleNewCount = (count, elements) => {
+//     console.log(count)
+
+//     // First visitor
+//     if (count.direction_in === 1 && )
+
+
+
+// }
+
+
+const initiatePage = (location, elements) => {
+    let start = 0
+    let stop = 0
+
+    firstVisitor(location).then(r => elements['firstVisitor'].innerHTML = r)
+    currentVisitors(location).then(r => elements['currentVisitors'].innerHTML = r)
+
+    // Today Visitors
+    if (elements['todayVisitors']) {
+        start = DateTime.now().startOf('day').toMillis()
+        stop = DateTime.now().toMillis()
+        countData(location, start, stop).then(r => elements['todayVisitors'].innerHTML = r)
+    }
+
+    // Week Visitors
+    if (elements['weekVisitors']) {
+        start = DateTime.now().startOf('week').toMillis()
+        stop = DateTime.now().toMillis()
+        countData(location, start, stop).then(r => elements['weekVisitors'].innerHTML = r)
+    }
+
+    // month Visitors
+    if (elements['monthVisitors']) {
+        start = DateTime.now().startOf('month').toMillis()
+        stop = DateTime.now().toMillis()
+        countData(location, start, stop).then(r => elements['monthVisitors'].innerHTML = r)
+    }
+
+    // year Visitors
+    if (elements['yearVisitors']) {
+        start = DateTime.now().startOf('year').toMillis()
+        stop = DateTime.now().toMillis()
+        countData(location, start, stop).then(r => elements['yearVisitors'].innerHTML = r)
+    }
+
+    // Night Owls
+    if (elements['nightOwls']) {
+        start = DateTime.now().startOf('year').toMillis()
+        stop = DateTime.now().toMillis()
+        countDataWithTime(location, start, stop, 6, 20).then(r => elements['nightOwls'].innerHTML = r)
+    }
+
+    return elements
+}
+
+
+
+const firstVisitor = async (location) => {
+    const { data, error } = await db
+        .from('counter')
+        .select('*')
+        .eq('location', location)
+        .eq('direction_in', 1)
+        .order('time', { ascending: false })
+        .limit(1)
+        .single()
+
+    if (error) { return 'No early birds' }
+
+    return DateTime.fromMillis(data.time).toFormat('HH:MM')
+}
+
+const currentVisitors = async (location) => {
+    let time = 0
+    // if it is between 24 and 03
+    if (DateTime.now().toFormat('H') > 0 && DateTime.now().toFormat('H') < 3) {
+        time = DateTime.now().plus({ days: -1 }).startOf('day').plus({ hour: 3 }).toMillis()
+    } else {
+        time = DateTime.now().startOf('day').plus({ hour: 3 }).toMillis()
+    }
+
+    const { data, error } = await db
+        .from('counter')
+        .select('*')
+        .eq('location', location)
+        .order('time', { ascending: true })
+        .gt('time', time)
+
+    if (error) { console.log(error) }
+
+    let visitors = 0
+    for (const count of data) {
+        visitors = visitors + count.direction_in - count.direction_out
+        if (visitors < 0) { visitors = 0 }
+    }
+
+    console.log({visitors: visitors})
+
+    return visitors
+}
+
+const countData = async (location, start, stop) => {
+    const { data, error, count } = await db
+        .from('counter')
+        .select('*', { count: 'exact', head: true })
+        .eq('location', location)
+        .eq('direction_in', 1)
+        .gt('time', start)
+        .lt('time', stop)
+
+    if (error) { console.log(error) }
+    return count
+}
+
+const countDataWithTime = async (location, start, stop, time1, time2) => {
+    const { data, error } = await db
+        .from('counter')
+        .select('*')
+        .eq('location', location)
+        .eq('direction_in', 1)
+        .gt('time', start)
+        .lt('time', stop)
+
+    if (error) { console.log(error) }
+
+    let counter = 0;
+    for (const count of data) {
+        const time = DateTime.fromMillis(count.time).toFormat('H');
+
+        if (time < time1 || time > time2) {
+            counter++
+        }
+    }
+    return counter
+}
+
+const getData = async (location, start, stop) => {
+    const { data, error } = await db
+        .from('counter')
+        .select('*')
+        .eq('location', location)
+        .gt('time', start)
+        .lt('time', stop)
+
+    if (error) { console.log(error) }
+    return data
+}
+
+const getInData = async (location, start, stop) => {
+    const { data, error } = await db
+        .from('counter')
+        .select('*')
+        .eq('location', location)
+        .eq('direction_in', 1)
+        .gt('time', start)
+        .lt('time', stop)
+
+    if (error) { console.log(error) }
+    return data
+}
+
+
+export { subscribe, initiatePage, getData, getInData }
